56cf57229594842de4849db64bf6c254f42da41a
Md Ismail Hosen <85634331+Ismail111111@users.noreply.github.com>
2022-12-26 09:11:19 +0600

    Add files via upload

--- a.cpp
+++ a.cpp
@@ -1,33 +1,55 @@
-#include<bits/stdc++.h>
-#define int long long int
-using namespace std;
-int32_t main(){
-    string s, t;
-    cin >> s >> t;
-    int n = s.size(), m = t.size();
-    int dp[n+1][m+1];
-    for(int i = 0; i <= n; i++){
-        for(int j = 0; j <= m; j++){
-            if(i==0 or j==0)dp[i][j] = 0;
-        }
-    }
+// Heap Sort in C++
 
-    for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= m; j++){
-            if(s[i-1] == t[j-1]){
-                dp[i][j] = 1 + dp[i-1][j-1];
-            }
-            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
-        }
+  #include <iostream>
+  using namespace std;
+
+  void heapify(int arr[], int n, int i) {
+    // Find largest among root, left child and right child
+    int largest = i;
+    int left = 2 * i + 1;
+    int right = 2 * i + 2;
+
+    if (left < n && arr[left] > arr[largest])
+      largest = left;
+
+    if (right < n && arr[right] > arr[largest])
+      largest = right;
+
+    // Swap and continue heapifying if root is not largest
+    if (largest != i) {
+      swap(arr[i], arr[largest]);
+      heapify(arr, n, largest);
     }
-    string st = "";
-    int i = n, j = m;
-    while(i > 0 and j > 0){
-        if(s[i-1] == t[j-1]){
-            st += s[i-1]; i--, j--;
-        }
-        else if(dp[i-1][j] > dp[i][j-1])i--;
-        else j--;
-    }reverse(st.begin(), st.end());
-    cout << st;
-}
+  }
+
+  // main function to do heap sort
+  void heapSort(int arr[], int n) {
+    // Build max heap
+    for (int i = n / 2 - 1; i >= 0; i--)
+      heapify(arr, n, i);
+
+    // Heap sort
+    for (int i = n - 1; i >= 0; i--) {
+      swap(arr[0], arr[i]);
+
+      // Heapify root element to get highest element at root again
+      heapify(arr, i, 0);
+    }
+  }
+
+  // Print an array
+  void printArray(int arr[], int n) {
+    for (int i = 0; i < n; ++i)
+      cout << arr[i] << " ";
+    cout << "\n";
+  }
+
+  // Driver code
+  int main() {
+    int arr[] = {1, 12, 9, 5, 6, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    heapSort(arr, n);
+
+    cout << "Sorted array is \n";
+    printArray(arr, n);
+  }
